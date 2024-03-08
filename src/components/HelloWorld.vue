f88c7d551953dc39a27519bedad27dd1688fc14a
xupan <xupan@autoai.com>
2024-03-08 19:06:27 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
