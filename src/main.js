389bc1682faefe5b3b980f88d6168816fd7a423f
xupan <xupan@autoai.com>
2024-03-13 20:01:07 +0800

    kkk

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import { createRouter,createWebHashHistory } from "vue-router";
+import router from './router';
 
 
 const app = createApp(App)
 app.use(ElementPlus);
-
+app.use(router)
 app.mount('#app')
