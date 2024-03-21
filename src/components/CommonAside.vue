608482ca8403c98d1a8bb19f6515644cf4c1f696
xp <realxp141414@163.com>
2024-03-22 00:20:49 +0800

    xxx

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -1,3 +1,79 @@
 <template>
-     <el-aside width="200px">Aside</el-aside>
+     <el-aside width="200px">
+          <el-menu class="el-menu-vertical-demo">
+               <el-menu-item :index="item.path" v-for="item in noChildren()" :key="item.path">
+                    <el-icon><component class="icons" :is="item.icon"></component></el-icon>
+                    <span>{{ item.label }}</span>
+               </el-menu-item>
+
+               <el-sub-menu index="1">
+                    <template #title>
+                         <el-icon>
+                              <location />
+                         </el-icon>
+                         <span>Navigator One</span>
+                    </template>
+                    <el-menu-item-group title="Group One">
+                         <el-menu-item index="1-1">item one</el-menu-item>
+                         <el-menu-item index="1-2">item two</el-menu-item>
+                    </el-menu-item-group>
+                    <el-menu-item-group title="Group Two">
+                         <el-menu-item index="1-3">item three</el-menu-item>
+                    </el-menu-item-group>
+                    <el-sub-menu index="1-4">
+                         <template #title>item four</template>
+                         <el-menu-item index="1-4-1">item one</el-menu-item>
+                    </el-sub-menu>
+               </el-sub-menu>
+
+
+          </el-menu>
+     </el-aside>
 </template>
+
+<script>
+export default {
+     data() {
+          return {
+               list: [
+                    {
+                         path: '/user',
+                         name: 'user',
+                         label: '用户管理',
+                         url: 'UserManager/UserManager',
+                         icon:'user'
+                    },
+
+                    {
+                         label: '其他',
+                         icon: 'location',
+                         children: [
+                              {
+                                   path: '/page1',
+                                   name: 'page1',
+                                   label: '页面1',
+                                   icon: 'setting',
+                                   url: 'Other/PageOne'
+                              },
+                              {
+                                   path: '/page2',
+                                   name: 'page2',
+                                   label: '页面2',
+                                   icon: 'setting',
+                                   url: 'Other/PageTwo'
+                              }
+                         ]
+                    }
+               ]
+          }
+     },
+     methods: {
+          noChildren() {
+               return this.list.filter((item) => !item.children)
+          },
+          hasChildren() {
+               return lisst.filter((item) => item.children)
+          }
+     }
+}
+</script>
--- src/components/CommonHead.vue
+++ src/components/CommonHead.vue
@@ -4,15 +4,28 @@
 
             <el-button size="small">
                 <el-icon :size="20">
-                    <Menu/>
+                    <Menu />
                 </el-icon>
             </el-button>
-
-
-
         </div>
         <div class="r-content">
-
+            <el-dropdown>
+    <span class="el-dropdown-link">
+      Dropdown List
+      <el-icon class="el-icon--right">
+        <arrow-down />
+      </el-icon>
+    </span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>Action 1</el-dropdown-item>
+        <el-dropdown-item>Action 2</el-dropdown-item>
+        <el-dropdown-item>Action 3</el-dropdown-item>
+        <el-dropdown-item disabled>Action 4</el-dropdown-item>
+        <el-dropdown-item divided>Action 5</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
         </div>
     </el-header>
 </template>
