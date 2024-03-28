823d3a09903ec68331fc1eea5b6bda63f693a4ab
xupan <xupan@autoai.com>
2024-03-28 19:45:42 +0800

    acc

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -1,32 +1,30 @@
 <template>
      <el-aside width="200px">
-          <el-menu class="el-menu-vertical-demo">
+          <el-menu class="el-menu-vertical-demo" background-color="#545c64" text-color="#fff"  :collapse="false">
                <el-menu-item :index="item.path" v-for="item in noChildren()" :key="item.path">
-                    <el-icon><component class="icons" :is="item.icon"></component></el-icon>
+                    <el-icon>
+                         <component class="icons" :is="item.icon"></component>
+                    </el-icon>
                     <span>{{ item.label }}</span>
                </el-menu-item>
 
-               <el-sub-menu index="1">
+               <el-sub-menu :index="item.label" v-for="item of hasChildren()" :key="item.path">
                     <template #title>
                          <el-icon>
-                              <location />
+                              <component class="icons" :is="item.icon"></component>
                          </el-icon>
-                         <span>Navigator One</span>
+                         <span>{{ item.label }}</span>
                     </template>
-                    <el-menu-item-group title="Group One">
-                         <el-menu-item index="1-1">item one</el-menu-item>
-                         <el-menu-item index="1-2">item two</el-menu-item>
+                    <el-menu-item-group>
+                         <el-menu-item :index="subItem.path" v-for="(subItem, subIndex) of item.children"
+                              :key="subIndex">
+                              <el-icon>
+                                   <component class="icons" :is="subItem.icon"></component>
+                              </el-icon>~
+                              <span>{{ subItem.label }}</span>
+                         </el-menu-item>
                     </el-menu-item-group>
-                    <el-menu-item-group title="Group Two">
-                         <el-menu-item index="1-3">item three</el-menu-item>
-                    </el-menu-item-group>
-                    <el-sub-menu index="1-4">
-                         <template #title>item four</template>
-                         <el-menu-item index="1-4-1">item one</el-menu-item>
-                    </el-sub-menu>
                </el-sub-menu>
-
-
           </el-menu>
      </el-aside>
 </template>
@@ -41,12 +39,13 @@ export default {
                          name: 'user',
                          label: '用户管理',
                          url: 'UserManager/UserManager',
-                         icon:'user'
+                         icon: 'user'
                     },
 
                     {
                          label: '其他',
                          icon: 'location',
+                         path: '/other',
                          children: [
                               {
                                    path: '/page1',
@@ -72,7 +71,7 @@ export default {
                return this.list.filter((item) => !item.children)
           },
           hasChildren() {
-               return lisst.filter((item) => item.children)
+               return this.list.filter((item) => item.children)
           }
      }
 }
