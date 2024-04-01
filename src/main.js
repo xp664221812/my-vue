5fd878005206f9fc1e358d1f16df5184f71f7d4a
xupan <xupan@autoai.com>
2024-04-01 20:32:52 +0800

    less

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from './router';
+import './assets/less/index.less'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
