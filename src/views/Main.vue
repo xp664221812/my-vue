abe714be124782cb5c0881cbcd209994b389ea5e
xupan <xupan@autoai.com>
2024-04-03 19:44:23 +0800

    ada

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,33 +1,46 @@
 <template>
-    <div class="common-layout">
-      <el-container >
-        <common-aside></common-aside>
-        <el-container class="r-container">
-          <common-header/>
-          <el-main>
-            <router-view/>
+  <div class="common-layout">
+    <el-container>
+      <common-aside></common-aside>
+      <el-container class="r-container">
+        <common-header />
+        <el-main>
+          <router-view />
 
-          </el-main>
-        </el-container>
+        </el-main>
       </el-container>
-    </div>
-  </template>
-  
+    </el-container>
+  </div>
+</template>
+
 
 <script>
 import CommonHeader from '../components/CommonHead.vue'
 import CommonAside from '../components/CommonAside.vue'
 
 export default {
-    components:{
-        CommonHeader,
-        CommonAside
-    }
+  components: {
+    CommonHeader,
+    CommonAside
+  }
 }
 </script>
 
-<style>
-  .r-container{
-    flex-wrap: wrap;
+<style lang="less">
+.r-container {
+  flex-wrap: wrap;
+}
+
+.common-layout {
+  height: 100%;
+
+  .el-container {
+    height: 100%;
+
+    .el-aside {
+      height: 100%;
+      background: #545c64;
+    }
   }
+}
 </style>
